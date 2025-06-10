f9e1278eb6f271be6bd0ec7db6d9319f11b4e73d
SantiagoCarranza <santiagocarranzazinny@gmail.com>
2025-06-10 17:33:53 -0300

    formula 1 change

--- formula1.s
+++ formula1.s
@@ -6,32 +6,40 @@
 formula1:
     PUSH    {R4-R7, LR}
     LDR     R6, =3000000
+    MOV     R4, #0x80
+    MOV     R5, #2
 
-    MOV     R4, #0x80       
-    MOV     R5, #2          
-
-ForFormula1:                
+ForFormula1:
     MOV     R0, R4
     BL      disp_binary
-
-    MOV     R0, R6          
+    MOV     R0, R6
     BL      delay
 
     MOV     R0, R4
-    MOV     R1, R5          
-    BL      div             
-    ADD     R4, R4, R0      
+    MOV     R1, R5
+    BL      div
+    ADD     R4, R4, R0
 
-    LSL     R5, R5, #1      
-    ADD     R5, R5, #2      
+    LSL     R5, R5, #1
+    ADD     R5, R5, #2
 
-    CMP     R5, #512        
-    BLT     ForFormula1     
+    CMP     R5, #512
+    BLT     ForFormula1
 
-    POP     {R4-R7, PC}     
-
+    POP     {R4-R7, PC}
 
 div:
-    UDIV    R0, R0, R1      
-    BX      LR     
-             
+    PUSH    {R2-R3, LR}
+    MOV     R2, #0
+    MOV     R3, R0
+
+div_loop:
+    CMP     R3, R1
+    BLT     div_end
+    SUB     R3, R3, R1
+    ADD     R2, R2, #1
+    B       div_loop
+
+div_end:
+    MOV     R0, R2
+    POP     {R2-R3, PC}
